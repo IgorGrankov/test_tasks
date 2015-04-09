2a2663dfa0da319326faf3fa96dcd3b5f2e08dce
Igor Grankov <igorgrnv@gmail.com>
2015-04-09 22:29:17 +0400

    Perfomance test for yahoo pages

---
+++ test_yahoo.py
@@ -0,0 +1,48 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+
+
+class YahooTest(unittest.TestCase):
+    links = []
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.get("http://www.yahoo.com")
+        self.browser.set_window_size(1280, 800)
+
+    def test_get_links(self):
+        more = self.browser.find_element(By.ID, 'uh-more-link')
+        more_button = self.browser.find_element_by_class_name('more-link')
+        actions = ActionChains(self.browser)
+        actions.move_to_element(more)
+        actions.click(more_button)
+        actions.perform()
+
+        items = self.browser.find_elements(By.XPATH,
+                                           '//*[@id="uh-more-link"]/ul/li/a')
+
+        for item in items:
+            self.links.append(item.get_attribute('href'))
+
+        for link in self.links:
+            start = datetime.now()
+            ff = webdriver.Firefox()
+            ff.get(link)
+            WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "fptoday-img")))
+            end = datetime.now()
+            load_time = end - start
+            self.assertTrue(load_time, 7)
+            ff.quit()
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
